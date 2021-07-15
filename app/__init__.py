973ceacf9c9b2d109e87bbaad6f8eaa339acbaca
1wickes <billeywong@gmail.com>
2021-07-16 01:21:01 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from .models import *
@@ -9,9 +9,18 @@ db = SQLAlchemy()
 def create_app(config=None):
     app = Flask(__name__, static_url_path="", static_folder="build")
     CORS(app)
-    app.config.from_object('config.ProductionConfig')
-    # app.config.from_object('config.DevelopmentConfig')
+    # app.config.from_object('config.ProductionConfig')
+    app.config.from_object('config.DevelopmentConfig')
     db.init_app(app)
+
+    # Serve React App
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + "/" + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, "index.html")
 
     from .auth import auth
     app.register_blueprint(auth)
